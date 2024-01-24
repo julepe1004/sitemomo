0ef991a21545eef04ab80eed5f8ca5132f3ce7d4
julepe1004 <157550230+julepe1004@users.noreply.github.com>
2024-01-24 10:03:03 -0300

    termina em 0

--- app.js
+++ app.js
@@ -4,10 +4,10 @@ const minutesElement = document.querySelector("[data-minutes]");
 const secondsElement = document.querySelector("[data-seconds]");
 
 const render = (days, hours, minutes, seconds) => {
-  daysElement.innerHTML = String(days).padStart("2", 0);
-  hoursElement.innerHTML = String(hours).padStart("2", 0);
-  minutesElement.innerHTML = String(minutes).padStart("2", 0);
-  secondsElement.innerHTML = String(seconds).padStart("2", 0);
+  daysElement.innerHTML = String(days).padStart(2, "0");
+  hoursElement.innerHTML = String(hours).padStart(2, "0");
+  minutesElement.innerHTML = String(minutes).padStart(2, "0");
+  secondsElement.innerHTML = String(seconds).padStart(2, "0");
 };
 
 const countdown = () => {
@@ -16,15 +16,20 @@ const countdown = () => {
 
   const timeLeft = targetDate - now;
 
-  const days = Math.floor(timeLeft / (1000 * 60 * 60 * 24));
-  const hours = Math.floor(
-    (timeLeft % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
-  );
-  const minutes = Math.floor((timeLeft % (1000 * 60 * 60)) / (1000 * 60));
-  const seconds = Math.floor((timeLeft % (1000 * 60)) / 1000);
+  if (timeLeft <= 0) {
+    render(0, 0, 0, 0);
+    clearInterval(countdownInterval);
+  } else {
+    const days = Math.floor(timeLeft / (1000 * 60 * 60 * 24));
+    const hours = Math.floor(
+      (timeLeft % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
+    );
+    const minutes = Math.floor((timeLeft % (1000 * 60 * 60)) / (1000 * 60));
+    const seconds = Math.floor((timeLeft % (1000 * 60)) / 1000);
 
-  render(days, hours, minutes, seconds);
+    render(days, hours, minutes, seconds);
+  }
 };
 
 countdown();
-setInterval(countdown, 1000);
+const countdownInterval = setInterval(countdown, 1000);
