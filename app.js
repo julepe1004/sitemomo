da2eb8f3025979f8e91f52b281f4c4c70e39588c
julepe1004 <157550230+julepe1004@users.noreply.github.com>
2024-11-03 15:19:32 -0300

    Update app.js

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ const render = (days, hours, minutes, seconds) => {
 
 const countdown = () => {
   const now = new Date();
-  const targetDate = new Date(2024, 9, 11, 9);
+  const targetDate = new Date(2024, 11, 28, 17);
 
   const timeLeft = targetDate - now;
 
